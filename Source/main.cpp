945c5f55b1d7d9fdd41a895943f2a7fe329a355e
SkyH1gh <95106202+SkyH1ghDev@users.noreply.github.com>
2024-11-10 19:26:46 +0100

    Updated main

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,5 +1,28 @@
-﻿#include <iostream>
+﻿#define _CRTDBG_MAP_ALLOC
 
-int main(int argc, char* argv[]) {
-	std::cout << "Yo World!";
+#include <crtdbg.h>
+
+#include <Windows.h>
+#include <d3d12.h>
+#include "DirectXMath.h"
+
+#include <iostream>
+
+namespace DX = DirectX;
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance,
+                      _In_ LPWSTR    lpCmdLine,
+                      _In_ int       nCmdShow)
+{
+
+	// Braces to let the application go out of scope before checking memory leaks
+	{
+		std::cout << "Test";
+	}
+
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	_CrtDumpMemoryLeaks();
+
+	return 0;
 }
