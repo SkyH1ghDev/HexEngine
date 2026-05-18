ae551518bee6f84b31f4d2659895fefcaffdedb9
Edvin Roth <99540868+ExR-dev@users.noreply.github.com>
2026-05-18 16:49:34 +0200

    Added MeshLoader shell file

---
+++ Engine/include/HexEngine/Assets/Mesh/MeshLoader.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace MeshLoader
+{
+	void LoadMesh(std::string_view filePath); // TODO: Give return type
+};
---
+++ Engine/src/HexEngine/Assets/Mesh/MeshLoader.cpp
@@ -0,0 +1,11 @@
+#include "MeshLoader.hpp"
+
+#include <DirectXMesh.h>
+#include <utils/WaveFrontReader.h>
+
+namespace DX = DirectX;
+
+void MeshLoader::LoadMesh(std::string_view filePath)
+{
+
+}
