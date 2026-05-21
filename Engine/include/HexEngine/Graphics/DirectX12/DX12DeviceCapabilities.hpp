641b9d9f65008cf39b099ee5f29fb65e78b5f8aa
SkyH1ghDev <contact@skyh1gh.dev>
2026-05-21 14:31:10 +0200

    Did stuffs teehee

---
+++ Engine/include/HexEngine/Graphics/DirectX12/DX12DeviceCapabilities.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+class DX12FeatureValidator
+{
+public:
+    DX12FeatureValidator() = default;
+    ~DX12FeatureValidator() = default;
+    DX12FeatureValidator(const DX12FeatureValidator&) = delete;
+    DX12FeatureValidator& operator=(const DX12FeatureValidator&) = delete;
+    DX12FeatureValidator(DX12FeatureValidator&&) = delete;
+    DX12FeatureValidator& operator=(DX12FeatureValidator&&) = delete;
+    
+    bool CheckTearingSupport();
+    
+private:
+    
+};
