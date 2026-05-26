cd9941fa2a9c2e5e699a82c7698192c97bfbe91f
Edvin Roth <99540868+ExR-dev@users.noreply.github.com>
2026-05-26 12:54:56 +0200

    Removed unused mesh definition

--- Engine/src/HexEngine/Graphics/DirectX12/DX12Renderer.cpp
+++ Engine/src/HexEngine/Graphics/DirectX12/DX12Renderer.cpp
@@ -13,26 +13,6 @@
 
 using namespace HexEngine::Graphics::DirectX12;
 
-struct VertexPosColor
-{
-	DirectX::XMFLOAT3 Position;
-	DirectX::XMFLOAT3 Color;
-};
-
-static VertexPosColor g_Vertices[8] = {
-	{ DirectX::XMFLOAT3( -1.0f, -1.0f, -1.0f ), DirectX::XMFLOAT3( 0.0f, 0.0f, 0.0f ) },  // 0
-	{ DirectX::XMFLOAT3( -1.0f, 1.0f, -1.0f ), DirectX::XMFLOAT3( 0.0f, 1.0f, 0.0f ) },   // 1
-	{ DirectX::XMFLOAT3( 1.0f, 1.0f, -1.0f ), DirectX::XMFLOAT3( 1.0f, 1.0f, 0.0f ) },    // 2
-	{ DirectX::XMFLOAT3( 1.0f, -1.0f, -1.0f ), DirectX::XMFLOAT3( 1.0f, 0.0f, 0.0f ) },   // 3
-	{ DirectX::XMFLOAT3( -1.0f, -1.0f, 1.0f ), DirectX::XMFLOAT3( 0.0f, 0.0f, 1.0f ) },   // 4
-	{ DirectX::XMFLOAT3( -1.0f, 1.0f, 1.0f ), DirectX::XMFLOAT3( 0.0f, 1.0f, 1.0f ) },    // 5
-	{ DirectX::XMFLOAT3( 1.0f, 1.0f, 1.0f ), DirectX::XMFLOAT3( 1.0f, 1.0f, 1.0f ) },     // 6
-	{ DirectX::XMFLOAT3( 1.0f, -1.0f, 1.0f ), DirectX::XMFLOAT3( 1.0f, 0.0f, 1.0f ) }     // 7
-};
-
-static WORD g_Indices[36] = { 0, 1, 2, 0, 2, 3, 4, 6, 5, 4, 7, 6, 4, 5, 1, 4, 1, 0,
-							   3, 2, 6, 3, 6, 7, 1, 5, 6, 1, 6, 2, 4, 0, 3, 4, 3, 7 };
-
 DX12Renderer::DX12Renderer(const HexEngine::SDL::SDLWindow& pWindow)
 {
 	#if defined(_DEBUG)
